97d62d930b5cdf11e3eb3a1def7836a9300d9e4a
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-11-30 10:33:37 +0000

    Start of the project

---
+++ spotify/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Sidebar from '../components/SideBar/Sidebar'
+
+export default function Home() {
+  return (
+    <div>
+  
+
+      
+
+      <main>
+        <Sidebar />
+        {/* content */}
+      </main>
+
+      {/* Player */}
+
+    </div>
+  )
+}
